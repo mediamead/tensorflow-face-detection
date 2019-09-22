40f1a2c238e31a4a66a4d61b27f1738e4d112c49
Alexandre Bezroutchko <alexandre.bezroutchko@gmail.com>
2019-09-22 13:44:47 +0200

    server.py: properly handle socket EOF

--- planb/server.py
+++ planb/server.py
@@ -28,7 +28,10 @@ while True:
 
     # Retrieve message size
     while len(data) < payload_size:
-        data += conn.recv(4096)
+        chunk = conn.recv(4096)
+        if chunk == b'':
+            raise RuntimeError("socket connection broken")
+        data += chunk
 
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
@@ -36,7 +39,10 @@ while True:
 
     # Retrieve all data based on message size
     while len(data) < msg_size:
-        data += conn.recv(4096)
+        chunk = conn.recv(4096)
+        if chunk == b'':
+            raise RuntimeError("socket connection broken")
+        data += chunk
 
     frame_data = data[:msg_size]
     data = data[msg_size:]
