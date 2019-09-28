0adaa145541d0fa5ded7a170cacd9f4788916519
mediamead <55883146+mediamead@users.noreply.github.com>
2019-09-28 21:23:08 +0300

    do not show visual by default

--- run-python-visual.bat
+++ run-python-visual.bat
@@ -2,6 +2,6 @@ call C:\Users\Workshop\Anaconda3\Scripts\activate tensorflow_gpu
 
 c:
 cd C:\Users\Workshop\Documents\GitHub\tensorflow-face-detection
-python.exe main.py -n -r -c 1
+python.exe main.py -r -c 1
 
 pause
