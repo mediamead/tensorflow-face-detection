ce9f95d933638b8cfcd44ea571975ead80c94994
mediamead <55883146+mediamead@users.noreply.github.com>
2019-09-30 00:48:36 +0300

    reduce GPU memory usage

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ class TensoflowFaceDector(object):
         with self.detection_graph.as_default():
             config = tf.ConfigProto()
             config.gpu_options.allow_growth = True
+            config.gpu_options.per_process_gpu_memory_fraction = 0.25
+            
             self.sess = tf.Session(graph=self.detection_graph, config=config)
             self.windowNotSet = True
 
